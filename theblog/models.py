5f2523fde9851dde1cd56607f8c66ac1df582b0a
Samer98 <samerhisham.work@gmail.com>
2023-09-12 21:29:33 +0300

    Adding profile page

--- theblog/models.py
+++ theblog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 from datetime import datetime,date
 # Create your models here.
 
@@ -15,10 +16,14 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-    body = models.TextField()
+    # body = models.TextField()
+    body = RichTextField(blank=True,null=True)
     post_date = models.DateField(auto_now_add=True)
-
     category = models.CharField(max_length=255,default='coding')
+    likes = models.ManyToManyField(User,related_name="blog_posts")
+
+    def total_likes(self):
+        return self.likes.count()
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
