1fecb96de9a15dccd5d0c9692ba3be6329a0d927
Samer98 <samerhisham.work@gmail.com>
2023-09-21 19:26:25 +0300

    add pages

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path , include
-from .views import UserRegisterView,UserEditView
+from .views import UserRegisterView,UserEditView ,ShowProfilePageView , EditProfilePageView
 
 urlpatterns = [
     path('register/',UserRegisterView.as_view(), name='register'),
     path('edit_profile/',UserEditView.as_view(), name='edit_profile'),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
 ]
